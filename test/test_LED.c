698d32cb06b8f85398cc55eeb9ba372cd0f06907
oussama <oussama.benbelhassen@ucare-watches.com>
2019-08-08 14:32:09 +0200

    test led

---
+++ test/test_LED.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "LED.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_LED_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement LED");
+}
